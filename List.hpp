09e26835d76b95dc68a34d9323f28d2d60184810
Nick Whalen <nlwhalen@umich.edu>
2025-11-18 16:01:58 -0500

    Completed list.hpp

--- List.hpp
+++ List.hpp
@@ -57,6 +57,14 @@ public:
   // will work correctly without defining these, you should omit them. A user
   // of the class must be able to create, copy, assign, and destroy Lists.
 
+  List();
+
+  ~List();
+
+  List(const List &other);
+
+  List & operator=(const List &rhs);
+
 private:
   //a private type
   struct Node {
@@ -71,6 +79,7 @@ private:
 
   Node *first;   // points to first Node in list, or nullptr if list is empty
   Node *last;    // points to last Node in list, or nullptr if list is empty
+  int list_size; // keeps track of list size
 
 public:
   ////////////////////////////////////////
@@ -81,7 +90,8 @@ public:
     // Add a default constructor here. The default constructor must set both
     // pointer members to null pointers.
 
-
+    Iterator()
+    : list_ptr(nullptr), node_ptr(nullptr) {}
 
     // Add custom implementations of the destructor, copy constructor, and
     // overloaded assignment operator, if appropriate. If these operations
@@ -109,6 +119,15 @@ public:
     // Note: comparing both the list and node pointers should be
     // sufficient to meet these requirements.
 
+    T & operator*() const;
+    
+    Iterator & operator++();
+
+    Iterator operator++(int);
+    
+    bool operator==(Iterator rhs) const;
+    
+    bool operator!=(Iterator rhs) const;
 
 
     // Type aliases required to work with STL algorithms. Do not modify these.
@@ -163,7 +182,7 @@ public:
 
 
     // add any friend declarations here
-
+    friend class List;
 
     // construct an Iterator at a specific position in the given List
     Iterator(const List *lp, Node *np);
@@ -197,6 +216,227 @@ public:
 // (your choice). Do not change the public interface of List, although you
 // may add the Big Three if needed.  Do add the public member functions for
 // Iterator.
+  template <typename T>
+  bool List<T>::empty() const {
+    return list_size == 0;
+  }
+
+  template <typename T>
+  int List<T>::size() const {
+    return list_size;
+  }
+
+  template <typename T>
+  T & List<T>::front() {
+    assert(!empty());
+    return first->datum;
+  }
+
+  template <typename T>
+  T & List<T>::back() {
+    assert(!empty());
+    return last->datum;
+  }
+
+  template <typename T>
+  void List<T>::push_front(const T &datum) {
+    Node *p = new Node;
+    p->datum = datum;
+    p->next = first;
+    p->prev = nullptr;
+    
+    if (first) {
+      first->prev = p;
+    }
+    else {
+      last = p;
+    }
+
+    first = p;
+    ++list_size;
+  }
+
+  template <typename T>
+  void List<T>::push_back(const T &datum) {
+    Node *p = new Node;
+    p->datum = datum;
+    p->prev = last;
+    p->next = nullptr;
+
+    if (last) {
+      last->next = p;
+    }
+    else {
+      first = p;
+    }
+
+    last = p;
+    ++list_size;
+  }
+
+  template <typename T>
+  void List<T>::pop_front() {
+    assert(!empty());
+    Node *to_delete = first;
+    first = first->next;
+
+    if (first) {
+      first->prev = nullptr;
+    }
+    else {
+      last = nullptr;
+    }
+    
+    delete to_delete;
+    --list_size;
+  }
+
+  template <typename T>
+  void List<T>::pop_back() {
+    assert(!empty());
+    Node *to_delete = last;
+    last = last->prev;
+
+    if (last) {
+      last->next = nullptr;
+    }
+    else {
+      first = nullptr;
+    }
+
+    delete to_delete;
+    --list_size;
+  }
+
+  template <typename T>
+  void List<T>::clear() {
+    while (!empty()) {
+      pop_front();
+    }
+  }
+
+  template <typename T>
+  void List<T>::copy_all(const List<T> &other) {
+    for (Node *np = other.first; np; np = np->next) {
+      push_back(np->datum);
+    }
+  }
+
+  template <typename T>
+  List<T>::List()
+  : first(nullptr), last(nullptr) {
+    list_size = 0;
+  }
+
+  template <typename T>
+  List<T>::~List() {
+    clear();
+  }
+
+  template <typename T>
+  List<T>::List(const List<T> &other) 
+    : first(nullptr), last(nullptr) {
+      copy_all(other);
+  }
+
+  template <typename T>
+  List<T>& List<T>::operator=(const List<T> &rhs) {
+    if (this == &rhs) {return *this;}
+    clear();
+    copy_all(rhs);
+    return *this;
+  }
+
+  template <typename T>
+  T & List<T>::Iterator::operator*() const {
+    assert(node_ptr);
+    return node_ptr->datum;
+  }
+
+  template <typename T>
+  typename List<T>::Iterator & List<T>::Iterator::operator++() {
+    assert(node_ptr);
+    node_ptr = node_ptr->next;
+    return *this;
+  }
+
+  template <typename T>
+  typename List<T>::Iterator List<T>::Iterator::operator++(int) {
+    Iterator copy = *this;
+    operator++();
+    return copy;
+  }
+
+  template <typename T>
+  List<T>::Iterator::Iterator(const List<T> *lp, Node *np)
+  : list_ptr(lp), node_ptr(np) {}
+
+  template <typename T>
+  bool List<T>::Iterator::operator==(Iterator rhs) const {
+    return (list_ptr == rhs.list_ptr) && (node_ptr == rhs.node_ptr);
+  }
+
+  template <typename T>
+  bool List<T>::Iterator::operator!=(Iterator rhs) const {
+    return !(*this == rhs);
+  }
+
+  template <typename T>
+  typename List<T>::Iterator List<T>::begin() const {
+    return Iterator(this, first);
+  }
+
+  template <typename T>
+  typename List<T>::Iterator List<T>::end() const {
+    return Iterator(this, nullptr);
+  }
+
+  template <typename T>
+  typename List<T>::Iterator List<T>::erase(Iterator i) {
+    Node *to_delete = i.node_ptr;
+    Node *next_n = to_delete->next;
+
+    if(to_delete->prev) {
+      to_delete->prev->next = to_delete->next;
+    }
+    else {
+      first = to_delete->next;
+    }
+    if(to_delete->next) {
+      to_delete->next->prev = to_delete->prev;
+    }
+    else {
+      last = to_delete->prev;
+    }
+
+    delete to_delete;
+    --list_size;
+    return Iterator(this, next_n);
+  }
+
+  template <typename T>
+  typename List<T>::Iterator List<T>::insert(Iterator i, const T &datum) {
+    Node *position = i.node_ptr;
+    Node *to_insert = new Node;
+    to_insert->datum = datum;
+    to_insert->next = position;
+
+    if(position) {
+      to_insert->prev = position->prev;
+      if (position->prev) {position->prev->next = to_insert;}
+      position->prev = to_insert;
+    }
+    else {
+      to_insert->prev = last;
+      if (last) last->next = to_insert;
+      last = to_insert;
+    }
+
+    if(!to_insert->prev) {first = to_insert;}
+    ++list_size;
+    return Iterator(this, to_insert);
+  }
+
 
 
 #endif // Do not remove this. Write all your code above this line.
